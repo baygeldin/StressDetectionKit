5cca4e3a07defa7dc3f59425ce114224b60f0990
Alexander Baygeldin <a.baygeldin@gmail.com>
2018-01-09 22:00:42 +0300

    Fix EventEmitter overloading.

--- src/device_kit.ts
+++ src/device_kit.ts
@@ -2,7 +2,7 @@ import { NativeModules, DeviceEventEmitter } from 'react-native'
 import { EventEmitter } from 'events'
 import { removeListener, removeAllListeners } from 'cluster';
 
-interface Device {
+export interface Device {
   id: String,
   address: String,
   name: String,
@@ -10,7 +10,7 @@ interface Device {
   manufacturer: String
 }
 
-interface Reading {
+export interface Reading {
   data: String,
   source: Device
 }
@@ -25,9 +25,13 @@ type COLLECTION_FINISHED = 'collectionFinished'
 type EVENTS = DATA | DEVICE_FOUND | DEVICE_CONNECTED | DEVICE_DISCONNECTED |
   AMBIGUOUS_DEVICE_FOUND | SCAN_FINISHED | COLLECTION_FINISHED
 
-// typeof EVENTS... compare to EVENTS array and throw error if anything.
+// TODO: typeof EVENTS... compare to EVENTS array and throw error if anything.
 
-declare class DeviceKitEmitter extends EventEmitter {
+const DeviceKitModule = NativeModules.DeviceKit
+const { EVENT_PREFIX, EVENTS } = DeviceKitModule
+
+// Wrapper for native DeviceKit with type declarations
+class DeviceKit extends EventEmitter {
   on(event: DATA, fn: (reading: Reading) => void): this
   on(event: DEVICE_FOUND, fn: (device: Device) => void): this
   on(event: DEVICE_CONNECTED, fn: (device: Device) => void): this
@@ -35,15 +39,18 @@ declare class DeviceKitEmitter extends EventEmitter {
   on(event: AMBIGUOUS_DEVICE_FOUND, fn: (device: Device) => void): this
   on(event: SCAN_FINISHED, fn: () => void): this
   on(event: COLLECTION_FINISHED, fn: () => void): this
-  removeListener(event: EVENTS, fn: (...args: any[]) => void): this
-  removeAllListeners(event: EVENTS): this
-}
+  on(event: string, fn: (...args: any[]) => void) {
+    return super.on(event, fn)
+  }
 
-const DeviceKitModule = NativeModules.DeviceKit
-const { EVENT_PREFIX, EVENTS } = DeviceKitModule
+  removeListener(event: EVENTS, fn: (...args: any[]) => void) {
+    return super.removeListener(event, fn)
+  }
 
-// Wrapper for native DeviceKit with type declarations
-class DeviceKit extends DeviceKitEmitter {
+  removeAllListeners(event: EVENTS) {
+    return super.removeAllListeners(event)
+  }
+
   init(key: string): Promise<void> {
     for (let e of EVENTS) {
       DeviceEventEmitter.addListener(`${EVENT_PREFIX}:${e}`, (d) => {
