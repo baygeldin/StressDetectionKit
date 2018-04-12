7760f1f298a5e7b5c570119251eed445c635af62
Alexander Baygeldin <a.baygeldin@gmail.com>
2018-04-12 19:44:56 +0300

    Draft ml with sklearn-porter

---
+++ ml/model.py
@@ -0,0 +1,19 @@
+import ipdb
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn_porter import Porter
+
+data = pd.read_json('test.json')
+
+x_train, x_test, y_train, y_test = train_test_split(data, data['state'], test_size=0.33)
+
+model = svm.SVC()
+model.fit(x_train, y_train)
+
+porter = Porter(model, language='js')
+output = porter.export(embed_data=True)
+print(output)
+
+ipdb.set_trace()
