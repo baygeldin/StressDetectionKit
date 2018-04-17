86c6520e5e43cc46680562b630edfc0932992cc4
Alexander Baygeldin <a.baygeldin@gmail.com>
2018-04-17 21:53:25 +0300

    Finish serialization and use PEP8

--- src/ml/model.py
+++ src/ml/model.py
@@ -11,20 +11,21 @@ root = os.path.dirname(os.path.realpath(__file__))
 samples_root = os.path.join(root, 'samples')
 samples_list = sorted(os.listdir(samples_root), key=lambda x: int(x))
 
+
 def train(samples=samples_list[-1], output='src/ml/model.pkl'):
     samples_path = os.path.join(samples_root, samples, 'samples.json')
     pkl_path = os.path.normpath(os.path.join(root, '../../', output))
-    
+
     data = pd.read_json(samples_path)
     x = pd.DataFrame(data['normalizedVector'].values.tolist())
     y = data['state']
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33)
-    
+
     model = svm.SVC()
     model.fit(x_train, y_train)
     joblib.dump(model, pkl_path)
-    
+
     # ipdb.set_trace()
 
 
