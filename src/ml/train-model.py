b556b0c883af560b03f990000983c688c443dc99
Alexander Baygeldin <a.baygeldin@gmail.com>
2018-05-07 00:58:48 +0300

    Improve model training
    Add stats (precision, recall, F1 with confidence intervals, confusion
    matrix).
    Add cross validation.
    Add hyper-parameters tuning.

--- src/ml/train-model.py
+++ src/ml/train-model.py
@@ -12,10 +12,14 @@ import ipdb
 import os
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
+#from yellowbrick.classifier import ROCAUC
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_validate, cross_val_predict, GridSearchCV
 from sklearn.externals import joblib
-from sklearn import svm
+from sklearn.utils import shuffle
+from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix, make_scorer
 
+# Arguments parsing
 arguments = docopt(__doc__)
 options, arguments = arguments
 output = options.output  # pylint: disable=E1101
@@ -33,19 +37,45 @@ if absent:
     print("Samples for \"%s\" don't exist." % absent)
     exit(1)
 
+kfolds = 10
+
+# Samples processing
 samples_paths = [os.path.join(samples_root, e, 'samples.json')
                  for e in entries]
-samples = pd.concat([pd.read_json(p) for p in samples_paths])
-
-pkl_path = os.path.normpath(os.path.join(root, '../../', output))
+samples = shuffle(pd.concat([pd.read_json(p) for p in samples_paths]))
 
 x = pd.DataFrame(samples['stdVector'].values.tolist())
 y = samples['state']
 
-x_train, _, y_train, _ = train_test_split(x, y, test_size=0.33)
+# Hyper-parameters tuning
+param_grid = {"kernel": ['linear', 'poly', 'rbf', 'sigmoid'],
+              "gamma": [1e-1, 1e-2, 1e-3, 1e-4],
+              "C": [1, 10, 100, 1000]}
+grid_search = GridSearchCV(SVC(), param_grid, cv=kfolds)
+grid_search.fit(x, y)
+model = grid_search.best_estimator_
 
-model = svm.SVC()
-model.fit(x_train, y_train)
+# Performance evaluation
+scorer = {
+    'f1_false': make_scorer(f1_score, pos_label=False),
+    'f1_true': make_scorer(f1_score, pos_label=True),
+    'precision_false': make_scorer(precision_score, pos_label=False),
+    'precision_true': make_scorer(precision_score, pos_label=True),
+    'recall_false': make_scorer(recall_score, pos_label=False),
+    'recall_true': make_scorer(recall_score, pos_label=True)
+}
+
+cv_results = cross_validate(model, x, y, cv=kfolds, scoring=scorer)
+
+for score in scorer.keys():
+    scores = cv_results["test_%s" % score]
+    print("%s accuracy: %0.2f (+/- %0.2f)" %
+          (score, scores.mean(), scores.std() * 2))
+
+print(confusion_matrix(y, cross_val_predict(model, x, y, cv=kfolds)))
+
+# Model persistence
+pkl_path = os.path.normpath(os.path.join(root, '../../', output))
 joblib.dump(model, pkl_path)
 
 # ipdb.set_trace()
