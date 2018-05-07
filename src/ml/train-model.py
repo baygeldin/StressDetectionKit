15a0857f0f2624665ace28ed7563991e5b73f57a
Alexander Baygeldin <a.baygeldin@gmail.com>
2018-05-07 20:42:37 +0300

    Fix model bias and representation
    Split train and test sets for grid search cross validation and performance evaluation cross validation.
    Then print it nicely in a table.

--- src/ml/train-model.py
+++ src/ml/train-model.py
@@ -12,12 +12,13 @@ import ipdb
 import os
 
 import pandas as pd
-#from yellowbrick.classifier import ROCAUC
+import numpy as np
 from sklearn.svm import SVC
-from sklearn.model_selection import cross_validate, cross_val_predict, GridSearchCV
+from sklearn.model_selection import cross_validate, cross_val_predict, \
+    GridSearchCV, train_test_split
+from sklearn.metrics import f1_score, precision_score, recall_score, \
+    confusion_matrix, make_scorer
 from sklearn.externals import joblib
-from sklearn.utils import shuffle
-from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix, make_scorer
 
 # Arguments parsing
 arguments = docopt(__doc__)
@@ -42,17 +43,25 @@ kfolds = 10
 # Samples processing
 samples_paths = [os.path.join(samples_root, e, 'samples.json')
                  for e in entries]
-samples = shuffle(pd.concat([pd.read_json(p) for p in samples_paths]))
+samples = pd.concat([pd.read_json(p) for p in samples_paths])
 
 x = pd.DataFrame(samples['stdVector'].values.tolist())
 y = samples['state']
 
+# Useful when trying to estimate how each feature contribute to the model:
+# x = pd.DataFrame(np.array([np.array(x) for x in samples['stdVector'].values])[:, (0, 1, 2)])
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33)
+
 # Hyper-parameters tuning
-param_grid = {"kernel": ['linear', 'poly', 'rbf', 'sigmoid'],
-              "gamma": [1e-1, 1e-2, 1e-3, 1e-4],
-              "C": [1, 10, 100, 1000]}
+param_grid = {
+    'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+    'gamma': [1e-1, 1e-2, 1e-3, 1e-4],
+    'C': [1, 10, 100, 1000]
+}
 grid_search = GridSearchCV(SVC(), param_grid, cv=kfolds)
-grid_search.fit(x, y)
+grid_search.fit(x_train, y_train)
+
 model = grid_search.best_estimator_
 
 # Performance evaluation
@@ -62,17 +71,31 @@ scorer = {
     'precision_false': make_scorer(precision_score, pos_label=False),
     'precision_true': make_scorer(precision_score, pos_label=True),
     'recall_false': make_scorer(recall_score, pos_label=False),
-    'recall_true': make_scorer(recall_score, pos_label=True)
+    'recall_true': make_scorer(recall_score, pos_label=True),
+    'f1_total': make_scorer(f1_score),
+    'precision_total': make_scorer(precision_score),
+    'recall_total': make_scorer(recall_score)
 }
 
-cv_results = cross_validate(model, x, y, cv=kfolds, scoring=scorer)
+cv_results = cross_validate(model, x_test, y_test, cv=kfolds, scoring=scorer)
+cv_predict = cross_val_predict(model, x_test, y_test, cv=kfolds)
 
-for score in scorer.keys():
-    scores = cv_results["test_%s" % score]
-    print("%s accuracy: %0.2f (+/- %0.2f)" %
-          (score, scores.mean(), scores.std() * 2))
+# Classification report
 
-print(confusion_matrix(y, cross_val_predict(model, x, y, cv=kfolds)))
+
+def confidence_interval(scores):
+    return "%0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
+
+
+df = [[confidence_interval(cv_results["test_%s_%s" % (row, col)])
+       for row in ['precision', 'recall', 'f1']] for col in ['true', 'false', 'total']]
+print(pd.DataFrame(df, [True, False, 'Total'], ['Precision', 'Recall', 'F1']))
+
+# Confusion matrix
+df = confusion_matrix(y_test, cv_predict)
+rows = ['True (actual)', 'False (actual)']
+cols = ['True (predicted)', 'False (predicted)']
+print(pd.DataFrame(df, rows, cols))
 
 # Model persistence
 pkl_path = os.path.normpath(os.path.join(root, '../../', output))
