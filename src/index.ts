72ed77ae997b7dbb7b66feb14e81d14fe7738739
Alexander Baygeldin <a.baygeldin@gmail.com>
2018-06-10 19:18:38 +0300

    Temporary fix for isMounted warning

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 
 import App from './app';
+
+// NOTE: Remove this line when updating React Native to a stable release where it's fixed.
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated'])
 
 AppRegistry.registerComponent('StressDetectionKit', () => App);
