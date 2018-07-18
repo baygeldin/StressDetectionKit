2ad90c19ba31cf1b5fac556cdb69edcf888f1b4f
Alexander Baygeldin <a.baygeldin@gmail.com>
2018-07-19 01:23:19 +0300

    Adapt foreground service for the latest Android (pass channel ID)

--- android/app/src/main/java/com/stressdetectionkit/ForegroundService.kt
+++ android/app/src/main/java/com/stressdetectionkit/ForegroundService.kt
@@ -1,11 +1,14 @@
 package com.stressdetectionkit
 
-import android.app.PendingIntent
-import android.app.Service
+import android.app.*
+import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import android.os.IBinder
 import android.os.PowerManager
+import android.support.annotation.RequiresApi
 import android.support.v4.app.NotificationCompat
 
 class ForegroundService : Service() {
@@ -15,7 +18,12 @@ class ForegroundService : Service() {
     }
 
     private val compatNotification by lazy {
-        val builder = NotificationCompat.Builder(this)
+        val builder =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationCompat.Builder(this, createNotificationChannel())
+                } else {
+                    NotificationCompat.Builder(this)
+                }
         val content = resources.getString(R.string.notification_content)
         builder
                 .setSmallIcon(R.drawable.splash_image)
@@ -26,8 +34,17 @@ class ForegroundService : Service() {
         val startIntent = Intent(applicationContext, MainActivity::class.java)
         val contentIntent = PendingIntent.getActivity(this, 1000, startIntent, 0)
         builder.setContentIntent(contentIntent)
-
         builder.build()
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        val NOTIFICATION_ID = 42
+        startForeground(NOTIFICATION_ID, compatNotification)
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
     }
 
     override fun onCreate() {
@@ -40,16 +57,16 @@ class ForegroundService : Service() {
         wakeLock.release()
     }
 
-    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        startForeground(NOTIFICATION_ID, compatNotification)
-        return START_STICKY
-    }
-
-    override fun onBind(intent: Intent): IBinder? {
-        return null
-    }
-
-    companion object {
-        private val NOTIFICATION_ID = 42
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(): String {
+        val channelId = "hello_world_service"
+        val channelName = "ForegroundService"
+        val channel = NotificationChannel(channelId, channelName,
+                NotificationManager.IMPORTANCE_NONE)
+        channel.lightColor = Color.BLUE
+        channel.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(channel)
+        return channelId
     }
 }
